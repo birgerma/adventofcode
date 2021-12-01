b79a3752f5fb5567f1e601fc5f6184c304225f66
Marcus Birgersson <marcus.s.birgersson@gmail.com>
2021-12-01 19:22:45 +0100

    add solution part2

--- 2021/01/main.py
+++ 2021/01/main.py
@@ -7,6 +7,12 @@ def read_file_lines(file_name):
         lines = f.read()
     return lines.split('\n')
 
+def as_ints(lst):
+    ints = []
+    for e in lst:
+        ints.append(int(e))
+    return ints
+
 def solve1():
     entries = read_file_lines(data_file)
 
@@ -18,6 +24,30 @@ def solve1():
 
     return count
 
+def solve2():
+    entries = read_file_lines(data_file)
+    entries = as_ints(entries)
+    count = 0
+
+    for i in range(1, len(entries)-2):
+        j=i+1
+        sum1 = sum(entries[i-1:i+2])
+        sum2 = sum(entries[j-1:j+2])
+        print(entries[i-1:i+2])
+        print(entries[j-1:j+2])
+        print(sum1, sum2)
+        if(sum2>sum1):
+            count+=1
+        # break
+
+    return count
+
+
+
 sol1 = solve1()
 assert sol1==1581
 print("Solution 1:", sol1)
+
+sol2 = solve2()
+# assert sol1==1581
+print("Solution 2:", sol2)
