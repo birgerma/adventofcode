ebef643fccf8bb008d140b7ad7355e5cedc36080
Marcus Birgersson <marcus.s.birgersson@gmail.com>
2022-12-01 10:59:38 +0100

    add correct solution to 1a

--- 2022/1/main.py
+++ 2022/1/main.py
@@ -7,4 +7,16 @@ sys.path.insert(0, '../')
 from common.io import *
 
 if __name__=='__main__':
-    print("Hello world!")
+    print("Solve for problem 1 day 1")
+    data = read_file('input.txt').split('\n')
+    sum = 0
+    maxSum = 0
+    for d in data:
+        if d=='':
+            print("New elf")
+            if(sum>maxSum):
+                maxSum = sum
+            sum=0
+        else:
+            sum+=int(d)
+    print(maxSum)
