6919634bed3f588d566192bfa23cf244e8da6b7e
Marcus Birgersson <marcus.s.birgersson@gmail.com>
2022-12-02 11:26:43 +0100

    add function for reading list from file

--- 2022/common/io.py
+++ 2022/common/io.py
@@ -3,3 +3,9 @@
 def read_file(fname):
     with open(fname,'r') as f:
         return f.read()
+
+def read_list_data(fname, splitter='\n'):
+    data = read_file(fname)
+    data = data.split(splitter)
+    data = [i for i in data if i] #Remove empty entries
+    return data
