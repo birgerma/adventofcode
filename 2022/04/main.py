0c266e4a92b3c66f7c3b25da4badd2299b8de38a
Marcus Birgersson <marcus.s.birgersson@gmail.com>
2022-12-04 12:13:44 +0100

    add solution for day 4 part B

--- 2022/04/main.py
+++ 2022/04/main.py
@@ -33,6 +33,10 @@ def is_in(p1, p2):
 def is_contained(p1, p2):
     return is_in(p1,p2) or is_in(p2,p1)
         
+
+def is_independent(p1,p2):
+  return p1[1]<p2[0] or p2[1]<p1[0]
+
 def partA(input):
     print("Solve for day {:d} part A".format(DAY))
     pair_list = format_input(input)
@@ -43,8 +47,17 @@ def partA(input):
     assert count==584
     print("Number of overlapping pairs:",count)
 
-def partB(item_list):
+def partB(input):
     print("Solve for day {:d} part B".format(DAY))
+    pair_list = format_input(input)
+    count = 0
+    for pair in pair_list:
+        if is_independent(pair[0], pair[1]):
+            count+=1
+    overlaps = len(pair_list)-count
+    assert overlaps==933
+    print("Number of overlapping pairs:",overlaps)
+    
 
 # 508 too low
 if __name__=='__main__':
@@ -52,6 +65,6 @@ if __name__=='__main__':
     #data_file = 'test.txt'
     item_list = read_list_data(data_file)
     partA(item_list)
-    #partB(item_list)
+    partB(item_list)
 
 
