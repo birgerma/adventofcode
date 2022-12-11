786a51dbd12544ced6ef1d6edc0dc8e63ab905b7
Marcus Birgersson <marcus.s.birgersson@gmail.com>
2022-12-11 15:42:53 +0100

    update template

--- 2022/template/main.py
+++ 2022/template/main.py
@@ -2,28 +2,62 @@
 import sys
 import os
  
+import argparse
 # adding common to the system path
 sys.path.insert(0, '../')
  
 from common.io import *
 
-DAY = int(os.getcwd().split('/')[-1])
+DAY = os.getcwd().split('/')[-1]
+
+def format_input(input):
+    return input
 
 def partA(input, expected=None):
-    print("Solve for day {:d} part A".format(DAY))
+    print("Solve for day {:} part A".format(DAY))
+    data = format_input(input)
     answear = None
+    
+    if answear:
+        print("Solution for day {:} part A:".format(DAY),answear)
     if expected:
         assert answear==expected
 
 def partB(input, expected=None):
-    print("Solve for day {:d} part B".format(DAY))
+    print("Solve for day {:} part B".format(DAY))
+    data = format_input(input)
+
     answear = None
+    if answear:
+        print("Solution for day {:} part B:".format(DAY),answear)
     if expected:
         assert answear==expected
 
+def get_input_data(fname, raw):
+    try:
+        if raw:
+            return read_file(fname)
+        return read_list_data(fname)
+    except FileNotFoundError as e:
+        print("Exception:", e)
+        return "" if raw else []
+
 if __name__=='__main__':
-    data_file = 'input.txt'
-    item_list = read_list_data(data_file)
-    partA(item_list)
-    partB(item_list)
+    parser = argparse.ArgumentParser(description='Advent of code day {:}'.format(DAY))
+    parser.add_argument('case', type=str, nargs='?', default='all', 
+                        choices=['all', 'a', 'A', 'b', 'B'],
+                        help='Solution for Advend of Code day')
+
+    parser.add_argument('--filename', '-f', type=str, nargs='?', default='input.txt')
+    parser.add_argument('--raw', type=bool, nargs=1, default=False)
+    args = parser.parse_args()
+    print("Args:", args)
+    input = get_input_data(args.filename, args.raw)
+    print("input:", input)
+    case = args.case.lower()
+    if case == 'a' or case == 'all':
+        partA(input)
+    if case == 'b' or case == 'all':
+        partB(input)
+
 
