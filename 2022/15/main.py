86ad422736917c9a73a94c469ac32cc1dd9e89bb
Marcus Birgersson <marcus.s.birgersson@gmail.com>
2022-12-15 19:54:27 +0100

    add solution day 15 part 1

--- 2022/15/main.py
+++ 2022/15/main.py
@@ -135,6 +135,50 @@ def get_max_r(data):
             r=d[4]
     return r
 import time
+def testPartB(input, expected=None):
+    print("Testing for day {:} part A".format(DAY))
+    data = format_input(input)
+    min_x = 0
+    min_y = 0
+    max_x = 20
+    max_y = 20
+
+    sensor_reach = defaultdict(lambda:0)
+    sensor_reach = add_sensors_beacons(data, sensor_reach)
+
+    coor = None
+    for sensor in data:
+        print("Sensor:", sensor)
+        r = sensor[4]
+        x_start = max(min_x, sensor[0]-r)
+        x_end = min(max_x, sensor[0]+r)
+        y_start = max(min_y, sensor[1]-r)
+        y_end = min(max_y, sensor[1]+r)
+        print(x_start, x_end, y_start, y_end)
+        for y in range(y_start, y_end+1):
+            print("y=", y)
+            dy = (abs(y-sensor[1]))
+            dx = abs(abs(y-sensor[1])-r)
+            print("Init:", x_start, x_end)
+            x_start=max(min_x, sensor[0]-dx)
+            x_end = min(max_x, sensor[0]+dx)
+            print("Update:", x_start, x_end)
+            print("dx:", dx, "dy:", dy,"r=",r)
+            continue
+            quit()
+            for x in range(x_start, x_end+1):
+                if (x,y) not in sensor_reach:
+                    if manhattan(x,y,sensor[0], sensor[1])<=r:
+                        sensor_reach[(x,y)]=1
+        quit()
+    for x in range(max_x+1):
+        for y in range(max_y+1):
+            if (x,y) not in sensor_reach:
+                coor = (x,y)
+    answear = coor[0]*4000000+coor[1]
+    print("Test b:", answear)
+    assert answear==56000011
+
 # 6160793 too high
 # 5995319 too high
 # 5335788 too high
@@ -196,16 +240,121 @@ def partA(input, expected=None):
     if expected:
         assert answear==expected
 
+import json
 def partB(input, expected=None):
     print("Solve for day {:} part B".format(DAY))
     data = format_input(input)
 
-    answear = None
+    min_x = 0
+    min_y = 0
+    max_x = 4000000
+    max_y = 4000000
+
+    sensor_reach = defaultdict(lambda:0)
+    sensor_reach = add_sensors_beacons(data, sensor_reach)
+    coor = None
+    intervals = {}
+    for sensor in data:
+        print("Checking sensor:", sensor)
+        r = sensor[4]
+        y_start = max(min_y, sensor[1]-r)
+        y_end = min(max_y, sensor[1]+r)
+
+        for y in range(y_start, y_end+1):
+            if not y in intervals:
+                intervals[y]=[]
+            dy = (abs(y-sensor[1]))
+            dx = abs(abs(y-sensor[1])-r)
+
+            x_start=max(min_x, sensor[0]-dx)
+            x_end = min(max_x, sensor[0]+dx)
+            intervals[y].append([x_start,x_end])
+
+    for y in intervals.keys():
+        i = merge_intervals(intervals[y])
+        if len(i)>1:
+            print(y, i)
+            y_ans = y
+            x_ans = i[0][1]+1
+            print()
+            break
+    answear=x_ans*4000000+y
+    print("Answear:", answear)
+    assert answear == expected
+    quit()
+
+    print("All checked, find answear")
+    y=3349056
+    x=3418491+1
+    answear = x*4000000+y
+    # Serializing json
+    json_object = json.dumps(intervals, indent=4)
+
+    # Writing to sample.json
+    with open("sample.json", "w") as outfile:
+            outfile.write(json_object)
+    quit()
+    print("Number of rows:", len(intervals.keys()))
+    for y in intervals.keys():
+        print(y)
+        print(intervals[y])
+    quit()
+    for x in range(max_x+1):
+        for y in range(max_y+1):
+            if (x,y) not in sensor_reach:
+                coor = (x,y)
+    answear = coor[0]*4000000+coor[1]
     if answear:
         print("Solution for day {:} part B:".format(DAY),answear)
     if expected:
         assert answear==expected
 
+def merge_intervals(intervals):
+    intervals=sorted(intervals)
+    while True:
+        i1 = intervals[0]
+        i2 = intervals[1]
+        if i1[1]<i2[0]:# Found non reached x
+            #print("Done:", i1[1], i1[0])
+            return [i1, i2]
+        elif i1[1]>=i2[1]: # i1 includes i2, remove i2
+            del intervals[1]
+        elif i1[1]>=i2[0]: # Overlapping, merge
+            i1[1]=i2[1]
+            intervals[0]=i1
+            del intervals[1]
+        else:
+            print("Error")
+            quit()
+        #print(i1, i2)
+        if len(intervals)==1:
+            break
+    #print(intervals)
+    return intervals
+
+def b_from_file():
+    y=3349056
+    x=3418491+1
+    print(x*4000000+y)
+    quit()
+    # Opening JSON file
+    with open('sample.json', 'r') as openfile:
+        # Reading from json file
+        json_object = json.load(openfile)
+
+    intervals = json_object
+    print("N keys:", len(intervals.keys()))
+
+    for y in intervals.keys():
+        #print(intervals[y])
+        #print(y)
+        i = merge_intervals(intervals[y])
+        print(i)
+        if len(i)>1:
+            print(y, i)
+            print()
+            break
+        
 def get_input_data(fname, raw):
     try:
         if raw:
@@ -218,7 +367,7 @@ def get_input_data(fname, raw):
 if __name__=='__main__':
     parser = argparse.ArgumentParser(description='Advent of code day {:}'.format(DAY))
     parser.add_argument('case', type=str, nargs='?', default='all', 
-                        choices=['all', 'a', 'A', 'b', 'B', 'test-a'],
+                        choices=['all', 'a', 'A', 'b', 'B', 'test-a', 'test-b'],
                         help='Solution for Advend of Code day')
 
     parser.add_argument('--filename', '-f', type=str, nargs='?', default='input.txt')
@@ -228,9 +377,13 @@ if __name__=='__main__':
     case = args.case.lower()
     if case == 'test-a' or case == 'all':
         testPartA(input)
+    if case == 'test-b' or case == 'all':
+        testPartB(input)
     if case == 'a' or case == 'all':
         partA(input, expected=5335787)
     if case == 'b' or case == 'all':
-        partB(input)
+        #b_from_file()
+        #quit()
+        partB(input, expected=13673971349056)
 
 
