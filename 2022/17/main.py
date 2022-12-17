ebf3bd518b36b69eb647997ebf444f3cd15a6691
Marcus Birgersson <marcus.s.birgersson@gmail.com>
2022-12-17 10:09:29 +0100

    read rocks from file

---
+++ 2022/17/main.py
@@ -0,0 +1,81 @@
+# importing sys
+import sys
+import os
+ 
+import argparse
+# adding common to the system path
+sys.path.insert(0, '../')
+ 
+from common.io import *
+
+DAY = os.getcwd().split('/')[-1]
+
+def format_input(input):
+    return input
+
+def get_rocks():
+    fname = "rocks.txt"
+    raw_rocks = read_file(fname).split('\n')
+
+    rock = []
+    rocks = []
+    for line in raw_rocks:
+        if line=='':
+            print("New rock")
+            rocks.append(rock)
+            rock=[]
+        else:
+            rock.append(list(line))
+            print(line)
+
+    return rocks
+def partA(input, expected=None):
+    print("Solve for day {:} part A".format(DAY))
+    data = format_input(input)
+    rocks = get_rocks()
+    print_list(rocks)
+    answear = None
+    
+    if answear:
+        print("Solution for day {:} part A:".format(DAY),answear)
+    if expected:
+        assert answear==expected
+
+def partB(input, expected=None):
+    print("Solve for day {:} part B".format(DAY))
+    data = format_input(input)
+
+    answear = None
+    if answear:
+        print("Solution for day {:} part B:".format(DAY),answear)
+    if expected:
+        assert answear==expected
+
+def get_input_data(fname, raw):
+    try:
+        if raw:
+            return read_file(fname)
+        return read_list_data(fname)
+    except FileNotFoundError as e:
+        print("Exception:", e)
+        return "" if raw else []
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser(description='Advent of code day {:}'.format(DAY))
+    parser.add_argument('case', type=str, nargs='?', default='all', 
+                        choices=['all', 'a', 'A', 'b', 'B'],
+                        help='Solution for Advend of Code day')
+
+    parser.add_argument('--filename', '-f', type=str, nargs='?', default='input.txt')
+    parser.add_argument('--raw', type=bool, nargs=1, default=False)
+    args = parser.parse_args()
+    print("Args:", args)
+    input = get_input_data(args.filename, args.raw)
+    print("input:", input)
+    case = args.case.lower()
+    if case == 'a' or case == 'all':
+        partA(input)
+    if case == 'b' or case == 'all':
+        partB(input)
+
+
