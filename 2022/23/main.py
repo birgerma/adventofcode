c1b74f742349a0f92d0b52550d868202204bf119
Marcus Birgersson <marcus.s.birgersson@gmail.com>
2022-12-23 18:50:38 +0100

    add solution day 23 part b

--- 2022/23/main.py
+++ 2022/23/main.py
@@ -20,20 +20,22 @@ def format_input(input):
     return map
 
 from collections import defaultdict
-def move_elfes(elf_map):
-    print("=== Round","INIT",'===')
-    draw_map(elf_map)
-    n_it = 10
+def move_elfes(elf_map, n_rounds=10):
+    #print("=== Round","INIT",'===')
+    #draw_map(elf_map)
     order_index = 0
     #for it in range(n_it):
-    it=1
-    while it<=n_it:
-        elfes= [coor for coor in elf_map if elf_map[coor]=='#']
-        #count = defaultdict(lambda: 0)
+    it=0
+    
+    elfes= [coor for coor in elf_map if elf_map[coor]=='#']
+    elfes = set(elfes)
+    while it<n_rounds:
+        it+=1
+        print("Round:", it)
+       # elfes= [coor for coor in elf_map if elf_map[coor]=='#']
         count = {}
         moves = []
         for x,y in elfes:
-            #print("x,y", x,y)
             nw = (x-1, y-1)
             n= (x, y-1)
             ne = (x+1, y-1)
@@ -51,50 +53,33 @@ def move_elfes(elf_map):
             ]
             next_dir = [n, s, w,e]
             if (not any([c in elfes for c in[nw, n, ne, s, se, sw, w, e]])):
-                print(x,y,"No elfes near, wont move")
                 continue
 
             new_coor = None
             for i in range(len(checks)):
                 index = (i+order_index)%len(checks)
                 check = checks[index]
-                print("Check", index)
                 if not any([c in elfes for c in check]):
-                    print("PASS check", index, 'try move',x,y,'to', next_dir[index])
                     new_coor = next_dir[index]
                     break
             if new_coor is None:
                 # Nowhere to move
                 continue
-            #if (not any([c in elfes for c in [n, nw, ne]])):
-            #         new_coor = n
-            #         print("Try move",x,y," NORTH to",new_coor)
-            #elif (not any([c in elfes for c in [s, se, sw]])):
-            #         new_coor = s
-            #         print("Try move",x,y," SOUTH to",new_coor)
-            #elif (not any([c in elfes for c in [w, nw, sw]])):
-            #         new_coor = w
-            #         print("Try move",x,y," WEST to",new_coor)
-            #elif (not any([c in elfes for c in [e, ne, se]])):
-            #         new_coor = e
-            #         print("Try move",x,y," EAST to",new_coor)
-            #else: # Do not suggest movement
-            #    continue
             count[new_coor] = 2 if new_coor in count else 1
             moves.append((x,y,new_coor[0], new_coor[1]))
+        if len(moves)==0:
+            # No one moved, quit
+            break
         for x,y,new_x, new_y in moves:
-            print("Count:", new_x, new_y, count[(new_x, new_y)])
             if count[(new_x, new_y)]==1:
-                print("Moving",x,y,"to", new_x, new_y)
-                elf_map[(x,y)]='.'
-                elf_map[(new_x, new_y)]='#'
-                #print("Old coor:", elf_map[(x,y)])
-                #print("New coor:", elf_map[(new_x, new_y)])
-        print("=== Round",it,'===')
-        draw_map(elf_map)
+                elfes.remove((x,y))
+                elfes.add((new_x,new_y))
+                #elf_map[(x,y)]='.'
+                #elf_map[(new_x, new_y)]='#'
+        #print("=== Round",it,'===')
+        #draw_map(elf_map)
         order_index+=1
-        it+=1
-    return elf_map
+    return elfes, it
 
 def draw_map(elf_map):
     min_x = min([c[0] for c in elf_map])
@@ -110,15 +95,15 @@ def draw_map(elf_map):
                 print('.', end='')
         print()
 
-def count_score(elf_map):
-    min_x = min([c[0] for c in elf_map if elf_map[c]=='#'])
-    max_x = max([c[0] for c in elf_map if elf_map[c]=='#'])
-    min_y = min([c[1] for c in elf_map if elf_map[c]=='#'])
-    max_y = max([c[1] for c in elf_map if elf_map[c]=='#'])
+def count_score(elfpos):
+    min_x = min([c[0] for c in elfpos])
+    max_x = max([c[0] for c in elfpos])
+    min_y = min([c[1] for c in elfpos])
+    max_y = max([c[1] for c in elfpos])
     count = 0
     for y in range(min_y, max_y+1):
         for x in range(min_x, max_x+1):
-            if (x,y) not in elf_map or elf_map[(x,y)]=='.':
+            if (x,y) not in elfpos:
             #if elf_map[(x,y)]=='.':
                 count+=1
     return count
@@ -127,9 +112,10 @@ def count_score(elf_map):
 def partA(input, expected=None):
     print("Solve for day {:} part A".format(DAY))
     data = format_input(input)
-    elf_map = move_elfes(data)
+    elf_map, n_rounds = move_elfes(data)
     score = count_score(elf_map)
     print("Score:", score)
+    print("N rounds:", n_rounds)
     answear = None
     
     if answear:
@@ -140,6 +126,11 @@ def partA(input, expected=None):
 def partB(input, expected=None):
     print("Solve for day {:} part B".format(DAY))
     data = format_input(input)
+    elf_map, n_rounds = move_elfes(data, n_rounds = float('inf'))
+    score = count_score(elf_map)
+    print("Score:", score)
+    print("N rounds:", n_rounds)
+    answear = None
 
     answear = None
     if answear:
@@ -170,6 +161,6 @@ if __name__=='__main__':
     if case == 'a' or case == 'all':
         partA(input, expected=4116)
     if case == 'b' or case == 'all':
-        partB(input)
+        partB(input, 984)
 
 
