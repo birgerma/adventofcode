3520edbdc146b814f3a30adbb8d1d460d6becc63
marcus <marcus@mail>
2025-12-04 21:45:31 +0100

    add template

---
+++ 2025/template.py
@@ -0,0 +1,12 @@
+# Correct day1:
+# Correct day2: 
+
+import os, sys
+sys.path.append('..')
+
+from lib.io import read_file
+
+fname = 'test'
+# fname = 'data'
+data = read_file(fname)[0].split(',')
+
